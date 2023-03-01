c4dc4de663256b0e931a68d8c880dd43d18ddaf0
Issou83 <issou8@gmail.com>
2023-03-01 13:18:16 +0100

    Page d'acceuil finalisée avec le style qui inclu quelque animation en css + REACT: Composant Bouton lateral de navigation sur les autres pages est créé et integrer à la page Home (des conditions gerissent l'apparition et la redirection du navigateur)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import Buttons from '../components/Buttons';
+import DynamicText from '../components/DynamicText';
+import Mouse from '../components/Mouse';
+import Navigation from '../components/Navigation';
+import SocialNetwork from '../components/SocialNetwork';
 
 const Home = () => {
     return (
         <div>
-            <h1>HOME</h1>
+            <Mouse />
+            <div className="home">
+            <Navigation />
+            <SocialNetwork />
+            <div className="home-main">
+                <div className="main-content">
+                    <h1>NK.WebDev</h1>
+                    <h2>
+                        <DynamicText />
+                    </h2>
+                </div>
+            </div>
+            <Buttons right={"/projet-1"} />
+            </div>
         </div>
     );
 };
