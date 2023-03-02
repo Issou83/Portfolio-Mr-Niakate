be1322397f6d40be79b04b2f6408b4c1c5788b09
Issou83 <issou8@gmail.com>
2023-03-02 09:57:03 +0100

    Pages de projets: réalisation des composants notamment boutons scroll de chaques pages pour la navigation, modifications du stytle

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,23 +1,23 @@
 export const projectsData = [
   {
     id: 1,
-    title: "Casta",
-    date: "Janvier 2020",
-    languages: ["React", "Php", "Sass"],
+    title: "Projet-KANAP-OPC",
+    date: "Janvier 2023",
+    languages: ["Javascript"],
     infos:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
-    img: "./assets/img/projet-2.jpg",
-    link: "http://www.google.com",
+      "Construction d'un site e-commerce. Implémentation du site incluant plusieurs fonctionnalités: controle du formulaire client avant envoie, affichage des produits groupés ou individuel selon la page après récupération de la data sur une API, récuperation et affichage du Numero de commande, le tout de maniére dynamique, ",
+    img: "./assets/img/projet-1.png",
+    link: "https://github.com/Issou83/Projet-KASA-OPC-P7",
   },
   {
     id: 2,
-    title: "Lyon Béton",
-    date: "Mars 2020",
-    languages: ["Symfony", "Vue"],
+    title: "Projet-Panthère-Pérformances-SEO",
+    date: "Mars 2022",
+    languages: ["HTML", "CSS"],
     infos:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
-    img: "./assets/img/projet-1.jpg",
-    link: "http://www.google.com",
+      "Optimisation d'un site site web existant. Amélioration du référencement part étapes : -Analyse de l' état actuel de SEO du site fourni. -Amélioration du SEO du site. -Comparaison des résultats entre le site initial et celui après modifications. Leviers utilisés : Accessibilité , paramètres SEO , performances.",
+    img: "./assets/img/projet-2.png",
+    link: "https://issou83.github.io/Projet-La-Panthere-OPC-P4/",
   },
   {
     id: 3,
