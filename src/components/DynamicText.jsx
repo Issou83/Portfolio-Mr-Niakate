bf7b4801983341b16ae965cc964f9794bf5d563e
Issou83 <issou8@gmail.com>
2024-03-17 16:07:08 +0100

    MAJ google ads

--- src/components/DynamicText.jsx
+++ src/components/DynamicText.jsx
@@ -1,53 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 const DynamiqueText = () => {
-    useEffect(() => {
-        const target = document.getElementById("text-target")
+  useEffect(() => {
+    const target = document.getElementById("text-target");
 
-        let array = ["E-commerce", "SEO" , "Vitrine", "API-Rest"];
-        let wordIndex = 0;
-        let letterIndex = 0;
+    let array = ["E-commerce", "SEO", "Vitrine", "API-Rest"];
+    let wordIndex = 0;
+    let letterIndex = 0;
 
-        const createLetter = () => {
-            const letter = document.createElement("span");
-            target.appendChild(letter);
+    const createLetter = () => {
+      const letter = document.createElement("span");
+      target.appendChild(letter);
 
-            letter.classList.add("letter");
-            letter.style.opacity = "0";
-            letter.style.animation = "anim 5s ease forwards";
-            letter.textContent = array[wordIndex][letterIndex];
+      letter.classList.add("letter");
+      letter.style.opacity = "0";
+      letter.style.animation = "anim 5s ease forwards";
+      letter.textContent = array[wordIndex][letterIndex];
 
-            setTimeout(() => {
-                letter.remove()
-            }, 2000)
+      setTimeout(() => {
+        letter.remove();
+      }, 2000);
+    };
+
+    const loop = () => {
+      setTimeout(() => {
+        if (wordIndex >= array.length) {
+          wordIndex = 0;
+          letterIndex = 0;
+          loop();
+        } else if (letterIndex < array[wordIndex].length) {
+          createLetter();
+          letterIndex++;
+          loop();
+        } else {
+          letterIndex = 0;
+          wordIndex++;
+          setTimeout(loop, 2000);
         }
- 
-        const loop = () => {
-            setTimeout(() => {
-                if (wordIndex >= array.length) {
-                    wordIndex = 0;
-                    letterIndex = 0;
-                    loop()
-                } else if (letterIndex < array[wordIndex].length) {
-                    createLetter();
-                    letterIndex++;
-                    loop()
-                } else {
-                    letterIndex = 0;
-                    wordIndex++;
-                    setTimeout(loop, 2000)
-                }
-            }, 80)
-        }
-        loop()
-    }, [])
+      }, 80);
+    };
+    loop();
+  }, []);
 
-    return (
-        <span className="dynamic-text">
-            <span>Création</span>
-            <span id="text-target"></span>
-        </span>
-    );
+  return (
+    <span className="dynamic-text">
+      <span>Création</span>
+      <span id="text-target"></span>
+    </span>
+  );
 };
 
 export default DynamiqueText;
